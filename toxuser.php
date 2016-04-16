844467f100b92050e82b39b13658dd2912133074
Frantisek Gajdusek <frantisek@gajdusek.net>
2016-04-16 21:18:08 +0200

    separate users to config file

---
+++ toxuser.php
@@ -0,0 +1,7 @@
+<?php
+//edit this to paste your id's
+$users = array(
+	"echobot" => "76518406F6A9F2217E8DC487CC783C25CC16A15EB36FF32E335A235342C48A39",
+	"yournick"=> "place tox id here",
+	"anothernick"=> "another tox id",
+);
